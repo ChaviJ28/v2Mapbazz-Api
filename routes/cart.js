7a617e7d29bdb4a2042cb6721f592dc831638ea4
Chavi Surujbhali <chavi.codevigor@gmail.com>
2022-06-17 19:24:26 +0400

    cart

---
+++ routes/cart.js
@@ -0,0 +1,50 @@
+let express = require("express"),
+    router = express.Router(),
+    bcrypt = require("bcrypt"),
+    middleware = require("../middleware/index"),
+    userdb = require("../models/user");
+
+router.post("/add-cart", middleware.checkUserAuth, async(req, res) => {
+      try {
+          if (req.body.data) {
+            var searchParams = req.body.auth.user.id;
+              var insertData = {
+                  product: req.body.data.product_id,
+                  quantity: req.body.data.qty,
+                  color: req.body.data.color,
+                  size: req.body.data.size
+              };
+              await userdb.updateOne(searchParams, {$push: {cart: insertData}} );
+              res.status(200).json({ success: "product added to cart" });
+          } else {
+              res.status(400).json({ error: "corrupt date, try again" });
+          }
+      } catch (err) {
+          console.log(err);
+          res.status(500).json({ error: "Please Try Again later" });
+      }
+  });
+
+
+  //geuter sipa li retourne 1 id r cart la,lerla faire accordingly !
+  //pancr faire sa la !
+  router.post("/delete-cart", middleware.checkUserAuth, async(req, res) => {
+    try {
+        if (req.body.data) {
+          var searchParams = req.body.auth.user.id;
+            var insertData = {
+                product: req.body.data.product_id,
+                quantity: req.body.data.qty,
+                color: req.body.data.color,
+                size: req.body.data.size
+            };
+            await userdb.updateOne(searchParams, {$push: {cart: insertData}} );
+            res.status(200).json({ success: "product added to cart" });
+        } else {
+            res.status(400).json({ error: "corrupt date, try again" });
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: "Please Try Again later" });
+    }
+});
